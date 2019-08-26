efdce083e372f7ce26d320b4677cf57951d2cd3a
Tolu Smith jr <smyxbrone@gmail.com>
2019-08-26 15:42:33 -0700

    new commit monday

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
-$('.carousel').carousel({
-    interval: 1000
-})
-document.querySelector("#areas"){
+// $('.carousel').carousel({
+//     interval: 1000
+// })
+// document.querySelector("#areas"){
 
-}
+// }
+// $(".log-in").click(function () {
+//     $(".signIn").addClass("active-dx");
+//     $(".signUp").addClass("inactive-sx");
+//     $(".signUp").removeClass("active-sx");
+//     $(".signIn").removeClass("inactive-dx");
+// });
+
+// $(".back").click(function () {
+//     $(".signUp").addClass("active-sx");
+//     $(".signIn").addClass("inactive-dx");
+//     $(".signIn").removeClass("active-dx");
+//     $(".signUp").removeClass("inactive-sx");
+// });
