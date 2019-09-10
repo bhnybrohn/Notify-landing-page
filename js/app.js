f7cb4e60a2028c3d7e6a74a28ffde335e4d58a0e
Tolu Smith jr <smyxbrone@gmail.com>
2019-09-10 04:02:24 -0700

    SIte

--- js/app.js
+++ js/app.js
@@ -1,22 +1,4 @@
-// $('.carousel').carousel({
-//     interval: 1000
-// })
-// document.querySelector("#areas"){
 
-// }
-// $(".log-in").click(function () {
-//     $(".signIn").addClass("active-dx");
-//     $(".signUp").addClass("inactive-sx");
-//     $(".signUp").removeClass("active-sx");
-//     $(".signIn").removeClass("inactive-dx");
-// });
-
-// $(".back").click(function () {
-//     $(".signUp").addClass("active-sx");
-//     $(".signIn").addClass("inactive-dx");
-//     $(".signIn").removeClass("active-dx");
-//     $(".signUp").removeClass("inactive-sx");
-// });
 
 let navbar = document.querySelector(".navbar");
 let sticky = navbar.offsetTop;
